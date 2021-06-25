3b3e5c42696f4eddd04fe21b09caa7cd1f4fa05d
Abhinav Shukla <abhinav2014shukla@gmail.com>
2021-06-25 18:42:56 +0530

    Made changes in UI for ProfileFragment.kt

--- time-saver/app/src/main/java/com/avenger/timesaver/ui/ProfileFragment.kt
+++ time-saver/app/src/main/java/com/avenger/timesaver/ui/ProfileFragment.kt
@@ -1,16 +1,23 @@
 package com.avenger.timesaver.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CompoundButton
+import android.widget.Switch
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.app.ActivityCompat.recreate
+import androidx.fragment.app.Fragment
 import com.avenger.timesaver.R
 import dagger.hilt.android.AndroidEntryPoint
+
 
 @AndroidEntryPoint
 
 class ProfileFragment : Fragment() {
+    private var darkModeSwitch: Switch? = null
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -18,6 +25,16 @@ class ProfileFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setDarkModeSwitch(view);
+    }
+
+    private fun setDarkModeSwitch(view: View) {
+        darkModeSwitch=view.findViewById(R.id.darkModeSwitch)
+
     }
 
 }

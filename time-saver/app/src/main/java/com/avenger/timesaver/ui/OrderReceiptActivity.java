5e86ca9368322e99e6feafb09232f6fecdae5d80
Abhinav Shukla <abhinav2014shukla@gmail.com>
2021-06-27 10:23:56 +0530

    UI for Order Confirmation done

---
+++ time-saver/app/src/main/java/com/avenger/timesaver/ui/OrderReceiptActivity.java
@@ -0,0 +1,15 @@
+package com.avenger.timesaver.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.avenger.timesaver.R;
+
+import android.os.Bundle;
+
+public class OrderReceiptActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_receipt);
+    }
+}
